4b89771a56579b5a7995246b2d244030eb7e1025
Tom Kaden <Tom-Kaden95@live.de>
2017-09-08 16:06:15 +0200

    + Commands Channel-Liste im Sys-Log

--- src/main/java/DiscordBot.java
+++ src/main/java/DiscordBot.java
@@ -1,26 +1,33 @@
+import net.dv8tion.jda.client.events.call.GenericCallEvent;
 import net.dv8tion.jda.core.AccountType;
 import net.dv8tion.jda.core.JDA;
 import net.dv8tion.jda.core.JDABuilder;
-import net.dv8tion.jda.core.entities.Message;
-import net.dv8tion.jda.core.entities.MessageChannel;
-import net.dv8tion.jda.core.entities.User;
+import net.dv8tion.jda.core.entities.*;
 import net.dv8tion.jda.core.events.ReadyEvent;
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.core.events.message.priv.PrivateMessageReceivedEvent;
 import net.dv8tion.jda.core.exceptions.RateLimitedException;
 import net.dv8tion.jda.core.hooks.ListenerAdapter;
+import net.dv8tion.jda.core.managers.ChannelManager;
+
 import javax.security.auth.login.LoginException;
+import java.util.List;
 
 public class DiscordBot extends ListenerAdapter{
 
     protected static JDA jda = null;
 
     public static void main(String[] args) throws
-            LoginException, RateLimitedException, InterruptedException{
+            LoginException, RateLimitedException, InterruptedException {
         jda = new JDABuilder(AccountType.BOT)
                 .setToken("MzQ3NjE0ODczMTc5NjUyMDk2.DHcalA.TSByOZIWPyT__nONi_Dwdnc5PNg")
                 .addEventListener(new DiscordBot())
                 .buildBlocking();
+
+        List<TextChannel> textChannels = jda.getTextChannels();
+        System.out.println(textChannels);
     }
+
 
     @Override
     public void onReady(ReadyEvent event) {
@@ -34,10 +41,26 @@ public class DiscordBot extends ListenerAdapter{
         User author = event.getAuthor();
         Message message = event.getMessage();
         MessageChannel channel = event.getChannel();
-        String msg = message.getContent();
+        String msg = message.getContent().toLowerCase();
 
-        if (msg.equals("!Ping") || msg.equals("!ping")){
-            channel.sendMessage("Pong!").queue();
+        if (msg.equals("!ping")){
+            channel.sendMessage("Pong!").complete();
+        }
+        if(msg.equals("!name")){
+            channel.sendMessage(author.getName()).complete();
+        }
+        if (msg.equals("hey")){
+            channel.sendMessage("That's pretty good").complete();
+        }
+        if (msg.equals("privaterchat")){
+            PrivateChannel privateChannel = author.openPrivateChannel().complete();
+            privateChannel.sendMessage("Private Message").complete();
+        }
+        if (msg.equals("discord bots zu schreiben macht spaß")){
+            channel.sendMessage("Ich mag es auch zu leben!").complete();
+        }
+        if (msg.equals("i need love")){
+            channel.sendMessage("Ich denke immer an dich :*").complete();
         }
     }
 }
