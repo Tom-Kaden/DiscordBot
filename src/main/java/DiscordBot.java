bdd3c35dbc5b33b7364562f5f4527d117eac5c20
Tom Kaden <Tom-Kaden95@live.de>
2017-09-12 15:42:47 +0200

    ChatLog funktional

--- src/main/java/DiscordBot.java
+++ src/main/java/DiscordBot.java
@@ -1,43 +1,43 @@
-import net.dv8tion.jda.client.events.call.GenericCallEvent;
 import net.dv8tion.jda.core.AccountType;
 import net.dv8tion.jda.core.JDA;
 import net.dv8tion.jda.core.JDABuilder;
 import net.dv8tion.jda.core.entities.*;
 import net.dv8tion.jda.core.events.ReadyEvent;
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
-import net.dv8tion.jda.core.events.message.priv.PrivateMessageReceivedEvent;
 import net.dv8tion.jda.core.exceptions.RateLimitedException;
 import net.dv8tion.jda.core.hooks.ListenerAdapter;
-import net.dv8tion.jda.core.managers.ChannelManager;
 
 import javax.security.auth.login.LoginException;
+import java.util.Collections;
 import java.util.List;
+import java.util.ListIterator;
 
 public class DiscordBot extends ListenerAdapter{
 
-    protected static JDA jda = null;
+    private static JDA jda = null;
+    private static List<TextChannel> textChannels;
+    private static MessageChannel channelBotLog;
 
     public static void main(String[] args) throws
             LoginException, RateLimitedException, InterruptedException {
         jda = new JDABuilder(AccountType.BOT)
-                .setToken("MzQ3NjE0ODczMTc5NjUyMDk2.DHcalA.TSByOZIWPyT__nONi_Dwdnc5PNg")
+                .setToken("MzU2Njg0NTk4MTMxODE4NDk2.DJe8AQ.xa8Am4JbSu5X2lEjztkLHMKWoPU")
                 .addEventListener(new DiscordBot())
                 .buildBlocking();
 
-        List<TextChannel> textChannels = jda.getTextChannels();
-        System.out.println(textChannels);
+        textChannels = jda.getTextChannels();
+        channelBotLog = textChannels.get(0);
     }
 
 
     @Override
     public void onReady(ReadyEvent event) {
         super.onReady(event);
-        System.out.print("API is ready");
+        System.out.println("API is ready");
     }
 
     @Override
     public void onMessageReceived(MessageReceivedEvent event) {
-        JDA jda = event.getJDA();
         User author = event.getAuthor();
         Message message = event.getMessage();
         MessageChannel channel = event.getChannel();
@@ -52,15 +52,32 @@ public class DiscordBot extends ListenerAdapter{
         if (msg.equals("hey")){
             channel.sendMessage("That's pretty good").complete();
         }
-        if (msg.equals("privaterchat")){
+        if (msg.equals("privatechat")){
             PrivateChannel privateChannel = author.openPrivateChannel().complete();
             privateChannel.sendMessage("Private Message").complete();
         }
-        if (msg.equals("discord bots zu schreiben macht spaß")){
-            channel.sendMessage("Ich mag es auch zu leben!").complete();
+        if(msg.equals("!pokeempi")){
+            channel.sendMessage("@empi#6165 poke").complete();
         }
-        if (msg.equals("i need love")){
-            channel.sendMessage("Ich denke immer an dich :*").complete();
+        if (msg.startsWith("!log")){
+            try {
+                StringBuilder stringBuilder = new StringBuilder();
+                String number = msg.substring(msg.lastIndexOf(" "), msg.length());
+                int amount = Integer.parseInt(number.replace(" ", "")) + 1;
+
+                MessageHistory messageHistory = channel.getHistory();
+                List<Message> messageList = messageHistory.retrievePast(amount).complete();
+                Collections.reverse(messageList);
+                messageList.remove(messageList.size()-1);
+                stringBuilder.append("```\n");
+                for (Message aMessageList : messageList) {
+                    stringBuilder.append(aMessageList.getAuthor().getName()).append(" - " ).append(aMessageList.getContent()).append("\n");
+                }
+                stringBuilder.append("```");
+                channelBotLog.sendMessage(stringBuilder.toString()).complete();
+            } catch (Exception e){
+                channel.sendMessage("```Please enter the Correct Command:\n\n!log 'amount'```").complete();
+            }
         }
     }
 }
